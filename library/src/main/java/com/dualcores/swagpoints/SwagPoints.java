2a5311289fa27e55eb6b682bc30425cc9dc3f009
Engine Bai <enginebai@gmail.com>
2016-08-14 10:12:50 +0800

    Add styleable attributes

---
+++ library/src/main/java/com/dualcores/swagpoints/SwagPoints.java
@@ -0,0 +1,21 @@
+package com.dualcores.swagpoints;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by enginebai on 2016/8/7.
+ */
+public class SwagPoints extends View {
+
+
+
+	public SwagPoints(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	private void initView() {
+
+	}
+}
